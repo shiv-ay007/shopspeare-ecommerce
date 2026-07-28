eeebe982654dc1b4b5a9c33c92e2cf5d4f67c709
shivam yadav <shivamyadav993671@gmail.com>
2026-07-28 10:24:36 +0530

    vercel fix

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,28 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/Home" element={<Home />} />
+
+            <Route path="/product" element={<Product />} />
             <Route path="/Product" element={<Product />} />
+
             <Route path="/about" element={<About />} />
+            <Route path="/About" element={<About />} />
+
             <Route path="/cart" element={<Cart />} />
+            <Route path="/Cart" element={<Cart />} />
+
             <Route path="/login" element={<Login />} />
+            <Route path="/Login" element={<Login />} />
+
             <Route path="/signup" element={<Signup />} />
+            <Route path="/Signup" element={<Signup />} />
+
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/Profile" element={<Profile />} />
+
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
           <Footer />
